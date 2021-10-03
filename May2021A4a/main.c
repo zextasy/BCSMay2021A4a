bb3a7510f78b1afc102093588f557b8273c0c526
zex <the_zex@yahoo.com>
2021-10-03 11:15:40 +0100

    Initial Commit

---
+++ May2021A4a/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  May2021A4a
+//
+//  Created by Ugochukwu Ezekwem on 03/10/2021.
+//  Copyright © 2021 Ugochukwu Ezekwem. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
